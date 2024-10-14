34298db507e30d9b845be8ccc8a788c0ccee160c
Jialu Li <jialu_li@g.harvard.edu>
2024-10-14 15:50:46 -0400

    add more tunning parameters for causal forest

--- R/itr_run_causal-forest.R
+++ R/itr_run_causal-forest.R
@@ -40,7 +40,8 @@ train_causal_forest <- function(dat_train) {
     training_data_elements_cf[["X_expand"]],
     training_data_elements_cf[["Y"]],
     training_data_elements_cf[["T"]],
-    num.trees = 2000
+    num.trees = 2000,
+    tune.parameters = c("sample.fraction", "mtry", "min.node.size", "alpha", "imbalance.penalty")
   )
   return(fit)
 }
