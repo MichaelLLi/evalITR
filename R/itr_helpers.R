9d4fd6eb1ec9173643122573681668576049d191
yvdc <yvandecasteele@fas.harvard.edu>
2025-02-09 02:14:42 -0500

    fix outcome converted to numeric due to model.matrix

--- R/itr_helpers.R
+++ R/itr_helpers.R
@@ -432,23 +432,52 @@ rename_interaction_terms <- function(interaction_df){
 
 
 
-# function to convert formula and create new variables
+# # function to convert formula and create new variables
+# convert_formula <- function(user_formula, data, treatment){
+
+#   # get the outcome variable name from the formula
+#   outcome <- all.vars(user_formula)[1]
+
+#   # get the covariates from the formula
+#   interaction_df <- model.matrix(user_formula, data)
+#   interaction_df <- rename_interaction_terms(interaction_df)
+
+#   # remove variable Intercept from covariates list by name
+#   covariates_vec <- colnames(interaction_df)
+#   covariates_vec <- covariates_vec[!covariates_vec %in% c("Intercept", paste0(treatment))]
+#   # combine the interaction_df with the original data frame
+#   new_data = data %>% dplyr::select(all_of(outcome))
+#   combined_data <- cbind(new_data, interaction_df)
+
+#   return(list(data = combined_data, covariates = covariates_vec, outcome = outcome))
+# }
+
+
 convert_formula <- function(user_formula, data, treatment){
 
-  # get the outcome variable name from the formula
+  # Extract outcome before `model.matrix()`
   outcome <- all.vars(user_formula)[1]
+  new_data <- data %>% dplyr::select(all_of(outcome))
 
-  # get the covariates from the formula
-  interaction_df <- model.matrix(user_formula, data)
+  # Convert to factor explicitly (if it's supposed to be categorical)
+  if (is.factor(data[[outcome]])) {
+    new_data[[outcome]] <- as.factor(new_data[[outcome]])
+  }
+
+  # Get covariates using model.matrix() (excluding outcome)
+  interaction_df <- model.matrix(user_formula, data) %>%
+    as.data.frame()  # Keep as a dataframe
+  
   interaction_df <- rename_interaction_terms(interaction_df)
 
-  # remove variable Intercept from covariates list by name
+  # Remove unwanted columns (Intercept, treatment)
   covariates_vec <- colnames(interaction_df)
   covariates_vec <- covariates_vec[!covariates_vec %in% c("Intercept", paste0(treatment))]
-  # combine the interaction_df with the original data frame
-  new_data = data %>% dplyr::select(all_of(outcome))
+
+  # Combine outcome (preserved as factor) with covariates
   combined_data <- cbind(new_data, interaction_df)
 
   return(list(data = combined_data, covariates = covariates_vec, outcome = outcome))
 }
+
 
